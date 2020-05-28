3dfbe119d7eab764e250e27044d9f2552267df27
Dmitriy A <astwmss2@mail.ru>
2020-05-28 15:47:33 +0300

    STEP 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mainApp',
+    'webexample',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
