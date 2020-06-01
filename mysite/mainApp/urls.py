402d98b75d3827a4c9a0f3ae75a08e8f9cb21fe5
Dmitriy A <astwmss2@mail.ru>
2020-06-01 13:54:59 +0300

    DAY 3

--- mysite/mainApp/urls.py
+++ mysite/mainApp/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='ingex'), 
+    url(r'^$', views.index, name='ingex'),
+    url(r'^contact/', views.contact, name='contact')
 ]
--- mysite/mainApp/views.py
+++ mysite/mainApp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mainApp/homePage.html')
+
+def contact(request):
+    return render(request, 'mainApp/basic.html', {'values' : ['наш телефон:', '8-800-2000-500']})
     
 
 # Create your views here.
